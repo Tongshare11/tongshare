99ac4a9e406c75b0646138996c01ab070ea0f9e8
Wyverald <wyverald@gmail.com>
2011-10-29 15:14:46 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tongshare::Application.config.secret_token = 'e1b1f4c106f452f2cecf99edb5e4bfb0d9ac383530359d6a4fe25ae4380df0c7763116c47bc3411f558b2404a1c69cc7b80da718947c5db1271b590afb4deb4f'
